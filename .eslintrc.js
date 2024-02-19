f40638e55c6c480f9174d6e8f8a573c52586a77e
Oganes <og.marckaryan@yandex.ru>
2024-02-19 18:44:41 +0300

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
+        "prettier"
     ],
     "overrides": [
         {
--- src/sum.js
+++ src/sum.js
@@ -1,4 +1,8 @@
-export function sum(a, b){
+export function sum(      a,      b){
     
-    return a + b;
+    return a + 
+    
+    
+    
+    b;
 }
